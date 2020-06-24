343d2db31700807b4a8876fb0dd4c554be5bb7c2
Fábio Batista do Amorim <batista.amorim@hotmail.com>
2020-06-23 22:44:34 -0300

    Add fatorial

---
+++ math/combinatorial-analysis/fatorial.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int fatorial(int number){
+
+  if((number-1)>0){
+    return number * fatorial(number-1);
+  }
+  return 1;
+}
+
+int main(){
+
+  int number;
+
+  printf("Enter with a number!");
+  scanf("%i",&number);
+
+/*
+  Thist methods is not recommended
+
+  int i = number;
+  int result = 1;
+
+  for(i;i>0;i--){
+    result *= i;
+  }
+
+  printf("The number digited: %i\n",number);
+  printf("The fatorial is: %i", result);
+
+*/
+
+  printf(" *The fatorial is: %i", fatorial(number));
+
+  printf("\n");
+  return 0;
+
+}
